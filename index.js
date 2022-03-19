2669e9ef1ae7da10f90c7e8e213c24962da2e74d
Surojit <81943886+thesurojit-das@users.noreply.github.com>
2022-03-19 13:18:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 function guess(){
-
     var x=document.getElementById("gen").value;
-    if(x>=100)
+    if(x>=101)
     alert("Please enter number in range 1-100 ")
+
     var ran=Math.floor(Math.random()*100+1)
     if(x==ran){
         document.getElementById("r").innerHTML="Congrats well played ";
     }
-    else if (x>=ran && x<=ran){
+    else{
         document.getElementById("r").innerHTML="Better luck next time ";
     }
-    else{
-        document.getElementById("r").innerHTML=" ";
-    }
+
 }
 function clean(){
     document.getElementById("r").innerHTML=" ";
